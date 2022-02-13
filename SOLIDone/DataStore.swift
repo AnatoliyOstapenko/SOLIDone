852919b06854eda81d395af154f3a9612ef1bf53
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2022-02-13 14:41:43 +0200

    Add Data Store class
    clear the main VC from data storage block code

---
+++ SOLIDone/DataStore.swift
@@ -0,0 +1,36 @@
+//
+//  DataStore.swift
+//  SOLIDone
+//
+//  Created by MacBook on 13.02.2022.
+//
+
+import UIKit
+import CoreData
+
+class DataStore {
+
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var coreData = [SolidCoreData]()
+    
+    // Save data to Core Data
+    func saveData(_ data: String) {
+        let item = SolidCoreData(context: context)
+        item.name = data
+        coreData.append(item)
+        do {
+            try context.save()
+        } catch { print("load data failed, error \(error)") }
+            
+    }
+    // Fetch data from Core Data
+    func loadData() {
+        do {
+            coreData = try context.fetch(SolidCoreData.fetchRequest())
+            // Print every name from storage
+            for index in coreData {
+                print("saved name is \(index.name ?? "no name")")
+            }
+        } catch { print("load data failed: \(error)")}
+    }
+}
--- SOLIDone/ViewController.swift
+++ SOLIDone/ViewController.swift
@@ -26,8 +26,9 @@ struct Source: Decodable {
 class ViewController: UIViewController {
 
     var coreData = [SolidCoreData]()
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     var networkService = NetworkService()
+    var dataStore = DataStore()
 
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
@@ -38,7 +39,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         saveButton.layer.cornerRadius = 25.0
-        loadData() // Core Data
+
+        dataStore.loadData()
         networkService.fetchData()
     }
 
@@ -46,7 +48,7 @@ class ViewController: UIViewController {
     
     func checkName() {
         guard let name = nameTextField.text, name != "" else { return alert()}
-        saveData(name)
+        dataStore.saveData(name)
         print("Name \(name) is saved")
         nameLabel.text = name
     }
@@ -63,35 +65,11 @@ class ViewController: UIViewController {
     @IBAction func nameTextFieldAction(_ sender: UITextField) {
         nameLabel.text = nameTextField.text
     }
-    
-    
+
     @IBAction func saveButtonPressed(_ sender: UIButton) {
         checkName()
     }
-    
-    // MARK:- Data Storage
-    
-    // Save data to Core Data
-    func saveData(_ data: String) {
-        let item = SolidCoreData(context: context)
-        item.name = data
-        coreData.append(item)
-        do {
-            try context.save()
-        } catch { print("load data failed, error \(error)") }
-            
-    }
-    // Fetch data from Core Data
-    func loadData() {
-        do {
-            coreData = try context.fetch(SolidCoreData.fetchRequest())
-            // Print every name from storage
-            for index in coreData {
-                print("saved name is \(index.name ?? "no name")")
-            }
-        } catch { print("load data failed: \(error)")}
-    }
- 
+
 }
 
 
