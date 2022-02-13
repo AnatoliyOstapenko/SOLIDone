8fdd66bdaaa3cf8d4b80451547bab9188be53795
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2022-02-13 21:41:36 +0200

    Add gitignore
    Hide key API

--- SOLIDone/Model/K.swift
+++ SOLIDone/Model/K.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 struct K {
-    static let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5da05c606b6846f7b5dbf3bd05653340"
     
     static func alert(_ message: String, _ viewController: UIViewController) {
         let alert = UIAlertController.init(title: "Warning", message: message, preferredStyle: .alert)
---
+++ SOLIDone/Model/Key.swift
@@ -0,0 +1,13 @@
+//
+//  Key.swift
+//  SOLIDone
+//
+//  Created by MacBook on 13.02.2022.
+//
+
+import Foundation
+
+struct Key {
+    static let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5da05c606b6846f7b5dbf3bd05653340"
+    
+}
